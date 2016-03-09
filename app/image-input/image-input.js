31461a29d5d6bf3feee148792a33f366ab6eb5da
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2016-03-09 10:52:22 -0300

    Resolves #1

--- app/image-input/image-input.js
+++ app/image-input/image-input.js
@@ -9,35 +9,39 @@ app.directive('imageInput', function() {
 
         controller: function($scope) {
             var config = {
-                width: 256,
-                height: 256
+                width: 256, height: 256
             };
+
+            function bind(image) {
+                $scope.ngModel = image.toDataURL();
+                $scope.$apply();
+            }
+
+            function crop(image) {
+                SmartCrop.crop(image, config, function(result) {
+                    var crop = result.topCrop;
+                    var canvas = document.createElement('canvas');
+                    var ctx = canvas.getContext('2d');
+
+                    canvas.width = config.width;
+                    canvas.height = config.height;
+
+                    ctx.drawImage(image,
+                        crop.x, crop.y, crop.width, crop.height,
+                        0, 0, canvas.width, canvas.height);
+
+                    bind(canvas);
+                });
+            }
 
             $scope.update = function(element) {
                 var file = element.files[0];
-                var reader = new FileReader();
 
-                reader.onload = function() {
-                    var image = new Image();
-                    image.src = reader.result;
-
-                    SmartCrop.crop(image, config, function(result) {
-                        var crop = result.topCrop;
-                        var canvas = document.createElement('canvas');
-
-                        canvas.width = config.width;
-                        canvas.height = config.height;
-
-                        canvas.getContext('2d').drawImage(image,
-                            crop.x, crop.y, crop.width, crop.height,
-                            0, 0, canvas.width, canvas.height);
-
-                        $scope.ngModel = canvas.toDataURL();
-                        $scope.$apply();
+                loadImage.parseMetaData(file, function(data) {
+                    loadImage(file, crop, {
+                        orientation: data.exif? data.exif.get('Orientation') : null
                     });
-                };
-
-                reader.readAsDataURL(file);
+                });
             }
         }
     };
