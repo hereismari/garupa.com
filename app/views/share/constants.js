db4f97f39d3931ca3968c58d9a9c7324c07746f1
Marianne Linhares <mariannelinharesm@gmail.com>
2016-03-09 02:19:00 -0300

    Página finalizada por enquanto - Organização das pastas - Adicionado angular para iteração com butões - Adicionado modal para informar o usuário que as ações foram realizadas

---
+++ app/views/share/constants.js
@@ -0,0 +1,20 @@
+app.constant('Day', {
+    SUN: 'dom',
+    MON: 'seg',
+    TUE: 'ter',
+    WED: 'qua',
+    THU: 'qui',
+    FRI: 'sex',
+    SAT: 'sab'
+});
+
+app.constant('Way', {
+    TO: 0,
+    FROM: 1
+});
+
+app.constant('ModalMessage', {
+	SEND_INVITATION : { title: 'Enviar solicitacao de carona', message: 'O usuario sera notificado que voce quer essa carona!'},
+	NOTIFY_ME : { title: 'Quero ser notificado', message: 'Voce sera notificado assim que uma carona desse tipo surgir!'},
+	MAKE_A_FRIEND : { title: 'Solicatacao de amizade', message: 'O usuario sera notificado que voce quer ser seu amigo!'}
+});
