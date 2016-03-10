fee60204b8ff11566d09b96bdd457f3c4feaa63a
ArthurEmanuelRodriguesCosta <arthur.costa@ccc.ufcg.edu.br>
2016-03-09 23:09:11 -0300

    lista de bairros agora apresentada ao usuario quando necessario escrever o nome do bairro e tabela um pouco mais organizada

--- app/views/CarpoolOffering/offering.js
+++ app/views/CarpoolOffering/offering.js
@@ -1,8 +1,9 @@
 app.directive('offering', function() {
     return {
         restrict: 'E',
-        scope: {ngModel: '='},
-        templateUrl: 'app/CarpoolOffering/offering.html',
+        scope: {},
+        templateUrl: 'app/views/CarpoolOffering/offering.html',
+        css: 'app/views/CarpoolOffering/offering.css',
 
         controller: function($scope) {
             $scope.carpool = {
@@ -12,9 +13,25 @@ app.directive('offering', function() {
                 time: ''
             };
 
+            $scope.dataSource = ["Acacio Figueiredo", "Alto Branco", "Bairro das Cidades", "Bela Vista", "Bodocongo", "Cachoeira", "Castelo Branco", "Catole", "Catole de Ze Ferreira", "Centenario", "Centro", "Conjunto Cinza", "Dinamerica ", "Distrito de Catole", "Distrito Industrial", "Estacao Velha", "Estreito", "Gloria", "Itarare", "Jardim Atalaia", "Jardim Borborema", "Jardim Paulistano", "Jardim Verdejante", "Jeremias", "Jose Pinheiro", "Lauritzen", "Liberdade", "Ligeiro", "Louzeiro", "Malvinas", "Mirante", "Monte Castelo", "Monte Santo", "Mutirao do Serrotao", "Nova Brasilia", "Novo Bodocongo", "Palmeira", "Pedregal", "Prata", "Presidente Medici", "Quarenta", "Ramadinha", "Sandra Cavalcante", "Santa Cruz", "Santa Rosa", "Santa Terezinha", "Santo Antonio", "Sao Jose", "Sao Jose da Mata", "Serrotao", "Sitio Estreito", "Sitio Lucas", "Tambor", "Tres Irmas", "Universitario", "Velame", "Vila Cabral"];
+
             $scope.$watch('carpool.from', function() {
                 $scope.carpool.to = 'UFCG';
             });
         }
     }
+});
+
+$(document).ready(function () {
+
+    //ativa o datepicker
+    $('#date').datepicker({
+        format: "dd/mm/yyyy"
+    });
+
+    //ativa o switch
+    $("#way-cb").bootstrapSwitch();
+
+    //ativa o timepicker
+    $('#time').timepicker();
 });
