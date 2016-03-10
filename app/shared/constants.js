6146cb9b0d966abbf1899eaf6b732a3a2d26cc1a
ArthurEmanuelRodriguesCosta <arthur.costa@ccc.ufcg.edu.br>
2016-03-10 19:51:40 -0300

    SWITCH FUNCIONANDO

---
+++ app/shared/constants.js
@@ -0,0 +1,15 @@
+
+app.constant('Day', {
+    SUN: 'Domingo',
+    MON: 'Segunda',
+    TUE: 'Terça',
+    WED: 'Quarta',
+    THU: 'Quinta',
+    FRI: 'Sexta',
+    SAT: 'Sabado'
+});
+
+app.constant('Way', {
+    TO: 0,
+    FROM: 1
+})
