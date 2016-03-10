23745aa2b6df97df086463d16f47386351fd1d10
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2016-03-10 00:02:58 -0300

    Refatoração do commit anterior

--- app/views/profile.js
+++ app/views/profile.js
@@ -2,15 +2,4 @@
 app.controller('profile', function($scope, $stateParams, Users) {
     $scope.user = Users.get($stateParams.uid);
     $scope.signed = (Users.loggedUser === $scope.user);
-
-    $scope.table = _.map([
-        ['Matrícula', 'id',                                                                                    ],
-        ['Nome',      'name',    'ie.: Fulaninho da Silva',                    '.{3,}'                         ],
-        ['Telefone',  'phone',   'ie.: (83) 99988-1122',                         '\\(\\d\\d\\) \\d{4,5}-\\d{4}'],
-        ['Endereço',  'address', 'ie.: Rua Tomé de Souza, 288, José Pinheiro', '.{5,}'                         ],
-        ['Email',     'email',   'ie.: fulano.silva@email.com',                '.+@.+\\..+'                    ],
-    ],
-        function(row) {
-            return { name: row[0], data: row[1], placeholder: row[2], pattern: row[3] };
-        });
 });
