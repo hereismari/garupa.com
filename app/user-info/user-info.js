92fecaca0a68b613be503d8f3aeaa9eaa3ce8b1b
artxdmaster <arthur.rodrigues@ccc.ufcg.edu.br>
2016-03-11 20:58:51 -0300

    Tela de notificações

---
+++ app/user-info/user-info.js
@@ -0,0 +1,60 @@
+
+app.directive('userInfo', function() {
+    return {
+        restrict: 'E',
+        scope: { user: '=' },
+        templateUrl: 'app/user-info/user-info.html',
+        css: 'app/user-info/user-info.css',
+
+        controller: function($scope, Users) {
+            $scope.canEdit = {
+                value: $scope.user === Users.loggedUser
+            };
+
+            function Row(name, data, placeholder, pattern) {
+                this.name = name;
+                this.data = data;
+                this.placeholder = placeholder;
+                this.pattern = pattern;
+            }
+
+            $scope.table = [
+                new Row('Matrícula', 'id'),
+                new Row('Nome',      'name',    'ie.: Fulaninho da Silva',     '.{3,}'),
+                new Row('Telefone',  'phone',   'ie.: (83) 99988-1122',        '\\(\\d\\d\\) \\d{4,5}-\\d{4}'),
+                new Row('Endereço',  'address', 'ie.: Rua Tomé de Souza, 288', '.{5,}'),
+                new Row('Email',     'email',   'ie.: fulano.silva@email.com', '.+@.+\\..+')
+            ];
+        }
+    };
+});
+
+app.directive('userInfoRow', function() {
+    return {
+        restrict: 'A',
+        scope: { row: '=userInfoRow', user: '=', canEdit: '=' },
+        templateUrl: 'app/user-info/user-info-row.html',
+
+        controller: function($scope, $element) {
+            $scope.mode = 'view';
+
+            $scope.submit = function() {
+                $scope.user[$scope.row.data] = $scope.input;
+                $scope.return();
+            }
+
+            $scope.edit = function() {
+                $scope.mode = 'edit';
+                $scope.canEdit.value = false;
+                $scope.input = $scope.user[$scope.row.data];
+                $element.addClass('selected');
+            }
+
+            $scope.return = function() {
+                $scope.mode = 'view';
+                $scope.canEdit.value = true;
+                $element.removeClass('selected');
+            }
+        }
+    };
+});
