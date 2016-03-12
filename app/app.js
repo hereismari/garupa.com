b43454a7e7f821416e9228def595128f4d181b05
artxdmaster <arthur.rodrigues@ccc.ufcg.edu.br>
2016-03-11 21:00:22 -0300

    Tela de notificações

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ app.config(function($stateProvider) {
 	$stateProvider
 		.state('notifications', {
 			url: '/notificacoes',
-			controller: 'notifications',
 			templateUrl: 'app/views/notifications.html',
 			css: 'app/views/notifications.css'
 		});
