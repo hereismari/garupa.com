cd5f4d8f292d3a30d56e959e1faadf8dfb6eb567
113210422 JEFERSON FERREIRA DA SILVA <jefersonfs@lcc2-26.lcc.ufcg.edu.br>
2016-04-19 17:21:05 -0300

    Modificacao dos atributos das notificacoes para comunicacao com o backend

--- static/app/views/notifications/controller.js
+++ static/app/views/notifications/controller.js
@@ -2,23 +2,12 @@
 app.controller('notifications', function($scope, $timeout) {
 
     $scope.notifications_list = [
-        { type    : "INVITATION",
-          sender  : "Someone",
-          mensage : "mensage",
-          url_img : "https://40.media.tumblr.com/20625bfafa453b4d628f7de4a5d7e14e/tumblr_nz7u21Qai41t4osjeo2_250.png",
-          comment : "O bigu foi tão bom que me emocionei.."},
-
-        { type    : "INVITATION",
-          sender  : "Someone",
-          mensage : "mensage",
-          url_img : "https://pbs.twimg.com/profile_images/435830531951837184/Z50DeEtx.jpeg",
-          comment : "O bigu foi tão bom que me emocionei.."},
-
-        { type    : "INVITATION",
-          sender  : "Someone",
-          mensage : "mensage",
-          url_img : "https://40.media.tumblr.com/20625bfafa453b4d628f7de4a5d7e14e/tumblr_nz7u21Qai41t4osjeo2_250.png",
-          comment : "O bigu foi tão bom que me emocionei.."}
+        { status  : 1,
+	  type    : "",
+          date    : "dd/mm/aa",
+          message : "message",
+          user    : {},
+        }
     ];
 
     $scope.delete = function(index) {
